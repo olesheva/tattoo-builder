fb2a06983a8681fefe876218332b005f9951b11c
Vladimir Litvinov <litvinovvo@gmail.com>
2023-09-20 22:41:40 +0400

    chore: add env for different base

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/tattoo-builder',
-  build: {
-    outDir: 'docs'
-  },
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './', '')
+  const base = env.VITE_BASE || '/'
+
+  return {
+    base,
+    build: {
+      outDir: 'docs'
+    },
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
